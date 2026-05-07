b1a226c8bf70fa440a13562e47707b0134fb3389
Oussonè <barabryann@gmail.com>
2026-05-07 05:27:32 +0000

    Ajout d'un premier fichier

---
+++ FonctionsTries.h
@@ -0,0 +1,15 @@
+#ifndef FONCTIONSTRIES_H
+#define FONCTIONSTRIES_H
+
+#include "Ordinateur.h"
+
+// prototype 5 de la fonction de repartition par spécialité
+void repartitonSpecialite(Tordinateur **listeOrdinateur, int nbreOrdi);
+
+// prototype 6 de la fonction de répartition par marque
+void repartitionMarque(Tordinateur **listeOrdinateur, int nbreOrdi);
+
+// prototype 7 de la fonction de tri par vitesse de processeurs
+void trier_parVitesse(Tordinateur **listeOrdinateur, int nbreOrdi);
+
+#endif
